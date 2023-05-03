10c802268a3ce6d7282ad603e9a98ae1d07c55b8
rderm <r.dermarkar@gmail.com>
2023-05-02 21:51:38 -0700

    convert time test passing

--- src/todoistAPI/utils.py
+++ src/todoistAPI/utils.py
@@ -6,6 +6,7 @@ def convert_time(time_str: str, user_timezone: str = "UTC") -> datetime:
     user_tz = ZoneInfo(user_timezone)
 
     time = datetime.datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%S.%fZ")
-    time = time.replace(tzinfo= user_tz)
+    time = time.replace(tzinfo=ZoneInfo("UTC"))
+    time = time.astimezone(user_tz)
 
     return time
